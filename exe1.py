0362f4b20bb2f986d444a9cb37dc44bace372391
zamraniwidad <zamraniwidad@gmail.com>
2022-10-08 13:17:04 +0200

    Q1

---
+++ exe1.py
@@ -0,0 +1,5 @@
+m = float(input("entrez la valeur de la masse de mercure en Kg"))
+mvolumique = 13.6
+v = m*10**3/mvolumique
+print("le volume versé de la masse de mercure est : ", v,"cm3")
+print("le volume versé de la masse de mercure est : ", v*10**-6 ,"m3")
