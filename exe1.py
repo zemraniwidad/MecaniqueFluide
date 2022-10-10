f351c7e21c29821c86b3bc3d7ec183d9881cdf70
zamraniwidad <zamraniwidad@gmail.com>
2022-10-10 23:49:16 +0200

    EXERCICE DU PARALLIPIPEDE

--- exe1.py
+++ exe1.py
@@ -1,16 +1,34 @@
-m = float(input("entrez la valeur de la masse de mercure en Kg"))
+m = float(input("entrez la valeur de la masse du mercure en kg"))
 mvolumique = 13.6
 v = m*10**3/mvolumique
 print("le volume versé de la masse de mercure est : ", v,"cm3")
 print("le volume versé de la masse de mercure est : ", v*10**-6 ,"m3")
-v = v*10**-6
 l = float(input(" entrez la largeur du parrallepipede "))
 L = float(input(" entrez la longueur du parrallepipede "))
 H = float(input(" entrez la hauteur du parrallepipede "))
 Vm = l*L*H
 T = int(input("entrez le nombre des tubes cylindriques :"))
 vt = (v-Vm)/T
-print(" la volume de mercure versé dans chaque tube est: ",vt)
+print(" le volume de mercure versé dans chaque tube est: ",vt,"m*3")
+s = 0.02
+H = vt/s
+print("la hauteur de mercure dans chaque tube est : " ,H,"m")
+g = 10
+h1 = 0.4
+h2 = 0.5
+delta  = h1+h2
+print("la valeur de delta h est : ",delta ,"m")
+S = l*L
+print("la section du parallipipede est :",S,"m*2")
+F = mvolumique*g*delta*S
+print("l'intensité de force qui s'exerce sur le fond horizentale du récipient est :",F,"N")
+
+
+
+
+
+
+
 
 
 
