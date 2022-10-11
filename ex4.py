34c5b276eee8373a5e56692c89818634bd4b6e03
zamraniwidad <zamraniwidad@gmail.com>
2022-10-12 00:34:40 +0200

    EXE 4

--- ex4.py
+++ ex4.py
@@ -2,5 +2,3 @@ P =float(input("entrez la valeur de la presssion en pascale"))
 S =float(input("entrez la valeur de la section"))
 F = P*S
 print("la valeur de la force exercé",F,"N")
-print("hello world")
-
