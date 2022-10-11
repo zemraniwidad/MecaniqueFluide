ff9d0697c231ae55e8051960f77ba55815543065
zamraniwidad <zamraniwidad@gmail.com>
2022-10-11 22:50:50 +0200

    EXERCICE THE THANK

--- exe2.py
+++ exe2.py
@@ -1,7 +1,10 @@
-r = 0.15/2
-p = 6.9*10**6
+r = float(input(" entrez la valeur du rayon en metre "))
 A = 2*3.14*r**2
 print("la valeur de area est :",A,"m*2")
-F = p*A
+P = 6.9*10**6
+F = P*A
 print("la valeur de la force inside the thank est :",F,"N")
-print("la valeur de h")
+
+
+
+
