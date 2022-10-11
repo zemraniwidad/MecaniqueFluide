a2cfa1fc5e770af3e24fc766b62978620cb03fb3
zamraniwidad <zamraniwidad@gmail.com>
2022-10-11 23:37:47 +0200

    EXE 4

---
+++ ex4.py
@@ -0,0 +1,5 @@
+P =float(input("entrez la valeur de la presssion en pascale"))
+S =float(input("entrez la valeur de la section"))
+F = P*S
+print("la valeur de la force exercé",F,"N")
+
--- exe3.py
+++ exe3.py
@@ -1,1 +1,17 @@
-print("")
+M = 10**3
+H = 80
+g = 9.80
+p = M*g*H
+print("la valeur de la pression",p,"P")
+L = float(input("entrez la valeur de la langueur en metre"))
+l = float(input("entrez la valeur de la largeur en metre" ))
+A = L*l
+print("la valeur de erea" ,A,"m**2")
+F = p*A
+print("la valeur de la force",F,"N")
+
+
+
+
+
+
